c6c6ce4ba79c40847544375ab346adcfd79514fe
pyo_awndi <154031276+piyo12052004@users.noreply.github.com>
2026-01-03 19:33:01 +0700

    fix widh google login

--- src/utils/useGoogleAuth.ts
+++ src/utils/useGoogleAuth.ts
@@ -48,7 +48,7 @@ export async function initGoogleLogin() {
             theme: "outline",
             size: "large",
             shape: "pill",
-            width: 320, 
+            width: 9999,
         });
     }
 
